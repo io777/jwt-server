3c0f98024edb0e578c97ab6673f928b5f2b90506
io777 <Shap.production@mail.ru>
2019-05-13 00:03:38 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,81 @@
+const fs = require('fs')
+const bodyParser = require('body-parser')
+const jsonServer = require('json-server')
+const jwt = require('jsonwebtoken')
+
+const server = jsonServer.create()
+const router = jsonServer.router('./database.json')
+const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
+
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+server.use(jsonServer.defaults());
+
+const SECRET_KEY = '123456789'
+
+const expiresIn = '1h'
+
+// Create a token from a payload 
+function createToken(payload){
+  return jwt.sign(payload, SECRET_KEY, {expiresIn})
+}
+
+// Verify the token 
+function verifyToken(token){
+  return  jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ?  decode : err)
+}
+
+// Check if the user exists in database
+function isAuthenticated({email, password}){
+  return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
+}
+
+function getUserData({email, password}){
+  return userdb.users.find(user => user.email === email && user.password === password);
+}
+
+
+server.post('/auth/login', (req, res) => {
+  const {email, password} = req.body
+  if (isAuthenticated({email, password}) === false) {
+    const status = 401
+    const message = 'Incorrect email or password'
+    res.status(status).json({status, message})
+    return
+  }
+  const access_token = createToken({email, password})
+  const user = getUserData({email, password});
+  const result = {
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      access_token: access_token,
+      expires_in: expiresIn
+    }
+  }
+  res.status(200).json(result)
+})
+
+server.use(/^(?!\/auth).*$/,  (req, res, next) => {
+  if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+    const status = 401
+    const message = 'Error in authorization format'
+    res.status(status).json({status, message})
+    return
+  }
+  try {
+     verifyToken(req.headers.authorization.split(' ')[1])
+     next()
+  } catch (err) {
+    const status = 401
+    const message = 'Error access_token is revoked'
+    res.status(status).json({status, message})
+  }
+})
+
+server.use(router)
+
+server.listen(3000, () => {
+  console.log('Run Auth API Server')
+})
